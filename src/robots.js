e60ec894cd34350a8b861bc3b1cd31d55ac35990
trungt19 <trungt@uw.edu>
2019-07-30 15:41:26 -0700

    first commit robofriends

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Tahj Angel Thai",
+    username: "PoopyPants",
+    email: "Tahjangelthai@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Tristan Gabriel Thai",
+    username: "OneShoeBoy",
+    email: "Ilikeoneshoe@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Trung Duc Thai",
+    username: "CoolPAPA",
+    email: "trxthai19@gmail.com"
+  },
+  {
+    id: 4,
+    name: "Janice Wassenaar",
+    username: "Bestgirlfriendeva",
+    email: "janicewa16@gmail.com"
+  },
+  {
+    id: 5,
+    name: "Miley Wassenaar",
+    username: "iliketurtles",
+    email: "iliketurtles@gmail.com"
+  },
+  {
+    id: 6,
+    name: "Sam Kim Thai",
+    username: "daBro",
+    email: "samthai3@gmail.com"
+  }
+];
